c531d82c63da2e04acd6e4dcaf98ddb7b65957f2
ylb <// Category#import "UIImage+ColorImage.h"#import "UIView+Util.h"#import "UIViewController+GetViewController.h"#import "UIViewController+CurrentVc.h"#import "UIButton+Badge.h"#import "UIBarButtonItem+Badge.h"// Tools#import "QYDateUtil.h"#import "QYMacro.h"#import "QYAFNetWork.h"#import "CommonTool.h"// Base#import "BaseViewController.h"#import "BaseNavigationController.h"#import "BaseWkWebViewController.h"#import "CommonCalendarVC.h"// ThirdFrame#import MagicalRecord/MagicalRecord.h#import MBProgressHUD/MBProgressHUD.h#import "MBProgressHUD+QY.h"#import MJRefresh/MJRefresh.h#import "CHTCollectionViewWaterfallLayout.h"#import "CocoaPickerViewController.h"#import AMapFoundationKit/AMapFoundationKit.h#import MAMapKit/MAMapKit.h#import AMapSearchKit/AMapSearchKit.h#import AMapLocationKit/AMapLocationKit.h// Product#import "AppMacro.h"#import "AppDelegate.h"#import "UserUtil.h"#import "ProjectUtil.h"#import "HomePageUtil.h"// Category#import "UIImage+ColorImage.h"#import "UIView+Util.h"#import "UIViewController+GetViewController.h"#import "UIViewController+CurrentVc.h"#import "UIButton+Badge.h"#import "UIBarButtonItem+Badge.h"// Tools#import "QYDateUtil.h"#import "QYMacro.h"#import "QYAFNetWork.h"#import "CommonTool.h"// Base#import "BaseViewController.h"#import "BaseNavigationController.h"#import "BaseWkWebViewController.h"#import "CommonCalendarVC.h"// ThirdFrame#import MagicalRecord/MagicalRecord.h#import MBProgressHUD/MBProgressHUD.h#import "MBProgressHUD+QY.h"#import MJRefresh/MJRefresh.h#import "CHTCollectionViewWaterfallLayout.h"#import "CocoaPickerViewController.h"#import AMapFoundationKit/AMapFoundationKit.h#import MAMapKit/MAMapKit.h#import AMapSearchKit/AMapSearchKit.h#import AMapLocationKit/AMapLocationKit.h// Product#import "AppMacro.h"#import "AppDelegate.h"#import "UserUtil.h"#import "ProjectUtil.h"#import "HomePageUtil.h"eric@ylbservices.com>
2018-08-28 10:07:42 +0800

    no message

---
+++ iOSPracticeDemo/iOSPracticeDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iOSPracticeDemo
+//
+//  Created by ylb on 2018/8/28.
+//  Copyright © 2018年 EricZ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ iOSPracticeDemo/iOSPracticeDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iOSPracticeDemo
+//
+//  Created by ylb on 2018/8/28.
+//  Copyright © 2018年 EricZ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
